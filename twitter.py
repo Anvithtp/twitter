3e26468a38f0d3b8fc119b5ed2e482158bff5a18
anvith-cpu <75164943+anvith-cpu@users.noreply.github.com>
2021-01-28 10:33:27 +0530

    Create twitter.py

---
+++ twitter.py
@@ -0,0 +1,21 @@
+Nithin Naik10:29 AM
+import tweepy
+import os
+
+consumer_key = os.getenv('consumer_key)
+consumer_secret = os.getenv('consumer_secret)
+access_token = os.getenv('access_token)
+access_token_secret = os.getenv('access_token_secret)
+
+auth =tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+
+import time
+while True:
+  user = api.get_user('anvith_tp')
+  n = user.followers_count
+  api.update_profile(name =f'anvith tp {n} followers ')
+print(f'ANVITH TP {n} Followers)
+  time.sleep(60
